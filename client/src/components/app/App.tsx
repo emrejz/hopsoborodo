95022243b8e2620f148b6fbe806dff21cfd65269
emre kayacan <emre.kayacan@msn.com>
2020-09-02 17:13:46 +0300

    init app file

---
+++ client/src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "pages/home/Home";
+
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
