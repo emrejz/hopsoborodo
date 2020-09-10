60ac3a7c3f926480414e372eee0cbcdfb75b80bd
emre kayacan <emre.kayacan@msn.com>
2020-09-10 17:31:26 +0300

    created constants

---
+++ client/src/constants/index.ts
@@ -0,0 +1,31 @@
+import { ITopLinkItem } from "interfaces/index";
+export const topLinks: ITopLinkItem[] = [
+  {
+    title: "Siparişlerim",
+    path: "siparislerim",
+  },
+  {
+    title: "Günün Fırsatları",
+    path: "gunun-firsat-teklifi",
+  },
+  {
+    title: "Yurt Dışı Satış",
+    path: "hepsiglobal",
+  },
+  {
+    title: "Bugün Teslimat",
+    path: "ayni-gun-kargo-teslimat",
+  },
+  {
+    title: "Kampanyalar",
+    path: "kampanyalar",
+  },
+  {
+    title: "Girişimci Kadınlar",
+    path: "girisimci-kadinlar",
+  },
+  {
+    title: "Çözüm Merkezi",
+    path: "cozummerkezi",
+  },
+];
