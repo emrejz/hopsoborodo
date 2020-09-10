284e1774e71cd7648d982081e1b63435ff4727cc
emre kayacan <emre.kayacan@msn.com>
2020-09-10 17:36:28 +0300

    created home page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Block from "styledComps/block";
 import HeaderLoading from "./headerLoading";
+import HeaderContent from "./headerContent";
 
 import "./index.scss";
 
 function Home() {
   return (
-    <Block className="homeContainer">
+    <Block col className="homeContainer">
       <HeaderLoading loading={true} />
+      <HeaderContent />
     </Block>
   );
 }
