6b6c7df417ce11157d9c88482a5c1a0a89043098
emre kayacan <emre.kayacan@msn.com>
2020-09-11 16:06:06 +0300

    top links header edited

--- client/src/pages/home/headerContent/topLinks/customLink/index.tsx
+++ client/src/pages/home/headerContent/topLinks/customLink/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Block from "styledComps/block";
 import Text from "styledComps/text";
+import DeliveryMenu from "./deliveryMenu";
 
 import "./index.scss";
 interface IProps {
@@ -13,18 +14,17 @@ interface IProps {
 const CustomLink: React.FC<IProps> = ({ item: { title, path } }) => {
   return (
     <Block mr="10px" ml="10px" shr="0" className={"customLinkContainer"}>
-      <Link
-        to={path}
-        className={title === "Bugün Teslimat" ? " deliveryLink" : ""}
-      >
+      <Link to={path} className={title === "Bugün Teslimat" ? " delivery" : ""}>
         <Text
           size="--smLink"
           clr={title === "Girişimci Kadınlar" ? "--topLinkOrange" : "--topLink"}
           wei="--w700"
+          className="title"
         >
           {title}
-          <div className="deliveryLinkMenu"></div>
+          <div className="menu"></div>
         </Text>
+        {title === "Bugün Teslimat" && <DeliveryMenu />}
         <div className="overlay"></div>
       </Link>
     </Block>
