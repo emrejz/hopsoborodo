8523346e6a2a03a0f367bc30676f593860393d0e
emre kayacan <emre.kayacan@msn.com>
2020-09-14 16:23:05 +0300

    new static data for quick menu

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -29,3 +29,14 @@ export const topLinks: ITopLinkItem[] = [
     path: "cozummerkezi",
   },
 ];
+export const quickMenuItems = [
+  "Elektronik",
+  "Moda",
+  "Ev, Yaşam, Kırtasiye, Ofis",
+  "Oto, Bahçe, Yapı Market",
+  "Anne, Bebek, Oyuncak",
+  "Spor, Outdoor",
+  "Kozmetik, Kişisel Bakım",
+  "Süpermarket, Pet Shop",
+  "Kitap, Müzik, Film, Hobi",
+];
