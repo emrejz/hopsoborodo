61a461dcd56edd8e5dcbb6dfc763d1eb3e6597c7
emre kayacan <emre.kayacan@msn.com>
2020-09-15 00:57:31 +0300

    encrypted password pre saved

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const User = new Schema({
@@ -16,4 +17,27 @@ const User = new Schema({
   },
 });
 
+User.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+    if (this.password && this.password.trim() && this.password.trim() > 3) {
+      bcrypt.hash(user.password, salt, null, function (err, hash) {
+        if (err) {
+          return next(err);
+        }
+
+        user.password = hash;
+        next();
+      });
+    } else {
+      return next(new Error("Password can be min 3 characters!"));
+    }
+  });
+});
 module.exports = mongoose.model("user", User);
