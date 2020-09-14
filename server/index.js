c682ce9c8566af4bb2995804b501e1346bad1fe0
emre kayacan <emre.kayacan@msn.com>
2020-09-14 22:47:29 +0300

    inclueded user model to graphql context

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const { ApolloServer } = require("apollo-server-express");
 const { importSchema } = require("graphql-import");
 const resolvers = require("./graphql/resolvers");
 const mongo = require("./db/mongo");
+const User = require("./models/user");
 
 mongo();
 const app = express();
 const server = new ApolloServer({
   typeDefs: importSchema("./graphql/index.graphql"),
   resolvers,
+  context: () => ({
+    User,
+  }),
 });
 
 server.applyMiddleware({ app });
