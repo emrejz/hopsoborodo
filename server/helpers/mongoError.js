753611e4f2b25198843729d7fc1a2b07ad20c888
emre kayacan <emre.kayacan@msn.com>
2020-09-14 22:49:40 +0300

    created to convert mongo errors

---
+++ server/helpers/mongoError.js
@@ -0,0 +1,13 @@
+module.exports = {
+  forUser: (message) => {
+    if (message.includes("E11000")) {
+      throw new Error("Username exist!");
+    }
+    if (message.includes("minimum")) {
+      throw new Error("Username must be between 3 or 10 characters!");
+    }
+    if (message.includes("maximum")) {
+      throw new Error("Username must be between 3 or 10 characters!");
+    }
+  },
+};
