af8c0350efc594a2285bfe7788b21d51523074c7
emre kayacan <emre.kayacan@msn.com>
2020-09-14 22:50:27 +0300

    user mutation worked good

--- server/graphql/resolvers/mutations/user/index.js
+++ server/graphql/resolvers/mutations/user/index.js
@@ -1,9 +1,12 @@
+const { forUser } = require("../../../../helpers/mongoError");
 module.exports = {
-  createUser: async (parent, args) => {
+  createUser: async (parent, { data }, { User }) => {
     try {
-      console.log(parent);
-      console.log(args);
+      console.log(data);
+      await User({ ...data }).save();
+      return "emre";
     } catch (error) {
+      forUser(error.message);
       throw error;
     }
   },
