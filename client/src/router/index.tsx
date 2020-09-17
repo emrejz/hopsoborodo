7f2cb3cdc2a7eed8587e8feef96e64e267b124f1
emre kayacan <emre.kayacan@msn.com>
2020-09-18 00:02:41 +0300

    added sing page to router

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "pages/home";
+import Sign from "pages/sign";
 
 const Root = () => {
   return (
@@ -8,6 +9,9 @@ const Root = () => {
       <Switch>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/sign">
+          <Sign />
         </Route>
       </Switch>
     </Router>
