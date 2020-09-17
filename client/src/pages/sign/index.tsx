87a2b568eb3df8a48201a96def08c8507505f3d9
emre kayacan <emre.kayacan@msn.com>
2020-09-18 00:02:21 +0300

    created sign page

---
+++ client/src/pages/sign/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Block from "styledComps/block";
+import Text from "styledComps/text";
+import SignIn from "./signIn";
+import SignUp from "./signUp";
+import { useHistory } from "react-router-dom";
+
+import "./index.scss";
+
+const Index = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const history = useHistory();
+  const _onClick = () => {
+    history.push("/");
+  };
+  return (
+    <Block col ai="center" bc="--primary" className="signPageContainer">
+      <Block flex="1" bc="primary" pt="34px" pb="56px" jc="center">
+        <Text
+          as="button"
+          clr="--orange"
+          wei="--w700"
+          size="2.8rem"
+          onClick={_onClick}
+        >
+          hopsoborodo
+        </Text>
+      </Block>
+      <Block className="formContainer">
+        <Block className="buttons">
+          <Text
+            as="button"
+            onClick={() => setIsLogin(true)}
+            bc={isLogin ? "--white" : "--signButtons"}
+          >
+            Giriş yap
+          </Text>
+          <Text
+            as="button"
+            onClick={() => setIsLogin(false)}
+            bc={isLogin ? "--signButtons" : "--white"}
+          >
+            Üye ol
+          </Text>
+        </Block>
+        {isLogin ? <SignIn /> : <SignUp />}
+        <Text as="button" className="submitButton">
+          {isLogin ? "Giriş yap" : "Üye ol"}
+        </Text>
+      </Block>
+    </Block>
+  );
+};
+
+export default Index;
