e85ab94ce8d1d0c8ee068ceb2dd15375255e2c4a
emre kayacan <emre.kayacan@msn.com>
2020-09-21 16:58:08 +0300

    created sign up page

--- client/src/pages/sign/signUp/index.tsx
+++ client/src/pages/sign/signUp/index.tsx
@@ -2,13 +2,30 @@ import React, { useState } from "react";
 import Block from "styledComps/block";
 import Text from "styledComps/text";
 import SignInput from "../signInput";
+import { ESignButtonType } from "interfaces/index";
 
 const Index = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
   return (
     <Block col ai="center" bc="--primary">
-      <SignInput placeHolderText={"Username"} />
-      <SignInput forPassword={true} placeHolderText={"Password"} />
-      <SignInput forPassword={true} placeHolderText={"Password"} />
+      <SignInput
+        kind={ESignButtonType.username}
+        val={username}
+        setVal={setUsername}
+      />
+      <SignInput
+        kind={ESignButtonType.password}
+        val={password}
+        setVal={setPassword}
+      />
+      <SignInput
+        kind={ESignButtonType.cPassword}
+        val={cPassword}
+        setVal={setCPassword}
+        samePassword={cPassword === password}
+      />
     </Block>
   );
 };
