05e2189c6413bd618cd38d7892cbe56f502bac42
emre kayacan <emre.kayacan@msn.com>
2020-09-22 17:57:19 +0300

    created ESignButtonType enum

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -2,8 +2,15 @@ export interface ITopLinkItem {
   title: string;
   path: string;
 }
+
 export enum ESignInputCase {
   start = "start",
   error = "error",
   success = "success",
+}
+
+export enum ESignButtonType {
+  username = "username",
+  password = "password",
+  cPassword = "cPassword",
 }
