c19e0e190c9c6c53982529fa544c646a102d87bc
emre kayacan <emre.kayacan@msn.com>
2020-09-22 17:39:36 +0300

    sign up page oket

--- client/src/pages/sign/signUp/index.tsx
+++ client/src/pages/sign/signUp/index.tsx
@@ -1,15 +1,46 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import Block from "styledComps/block";
 import Text from "styledComps/text";
+import * as Icon from "components/icons";
 import SignInput from "../signInput";
+import SignError from "../signError";
 import { ESignButtonType } from "interfaces/index";
+import { useMutation } from "@apollo/client";
+import { createUser } from "graphql/user";
 
 const Index = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [cPassword, setCPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const history = useHistory();
+  const [_createUser] = useMutation(createUser);
+
+  const _onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const {
+        data: {
+          createUser: { token },
+        },
+      } = await _createUser({ variables: { username, password } });
+      setLoading(false);
+      localStorage.setItem("token", token);
+      history.push("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  const _disabled = (): boolean =>
+    username.length < 3 || password.length < 3 || password !== cPassword;
+
   return (
-    <Block col ai="center" bc="--primary">
+    <Block as="form" col ai="center" bc="--primary" onSubmit={_onSubmit}>
+      {error && <SignError error={error} setError={setError} />}
       <SignInput
         kind={ESignButtonType.username}
         val={username}
@@ -26,6 +57,14 @@ const Index = () => {
         setVal={setCPassword}
         samePassword={cPassword === password}
       />
+      <Block
+        as="button"
+        className="submitButton"
+        type="submit"
+        disabled={_disabled()}
+      >
+        {loading ? <Icon.LoadingCircle /> : <Text>Üye ol</Text>}
+      </Block>
     </Block>
   );
 };
