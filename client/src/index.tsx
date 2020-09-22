322d777f5b07ba4e9ab23a36dcb07aac06efd36e
emre kayacan <emre.kayacan@msn.com>
2020-09-22 17:38:21 +0300

    provided app by apollo graphql

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/app";
-
+import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
 import "./index.scss";
 
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_SERVER_URL,
+  cache: new InMemoryCache(),
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
