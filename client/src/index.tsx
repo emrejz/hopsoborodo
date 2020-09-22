893ed1d2e993106616e1ede634b648b0c3d1b156
emre kayacan <emre.kayacan@msn.com>
2020-09-22 18:53:33 +0300

    added token on every request to server

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/app";
-import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
+import {
+  ApolloProvider,
+  InMemoryCache,
+  ApolloClient,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 import "./index.scss";
 
-const client = new ApolloClient({
+const link = createHttpLink({
   uri: process.env.REACT_APP_SERVER_URL,
+  credentials: "include",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  console.log(token);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : "",
+    },
+  };
+});
+const client = new ApolloClient({
   cache: new InMemoryCache(),
+  link: authLink.concat(link),
 });
 ReactDOM.render(
   <React.StrictMode>
