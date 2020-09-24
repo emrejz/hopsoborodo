8d2b7eae41c367c2c1b3b1b5461545b88b904ec5
emre kayacan <emre.kayacan@msn.com>
2020-09-25 01:27:50 +0300

    removed console.log

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -18,7 +18,6 @@ const link = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("token");
-  console.log(token);
   return {
     headers: {
       ...headers,
