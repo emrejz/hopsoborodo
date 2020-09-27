4b5cfb9ff6de479e6ca1e61a25fd359cb00bb697
emre kayacan <emre.kayacan@msn.com>
2020-09-27 12:19:46 +0300

    changed graphql folder name and structer

---
+++ client/src/graphqlResolvers/carousel/index.ts
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+// export const loginUser = gql`
+//   query($username: String!, $password: String!) {
+//     loginUser(data: { username: $username, password: $password }) {
+//       token
+//     }
+//   }
+// `;
+// export const activeUser = gql`
+//   query {
+//     activeUser {
+//       username
+//     }
+//   }
+// `;
