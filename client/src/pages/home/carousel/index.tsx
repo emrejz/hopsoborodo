38d9bbfb28176f8b12fe483944556d7c48045541
emre kayacan <emre.kayacan@msn.com>
2020-10-01 00:55:45 +0300

    carousel  comp created

--- client/src/pages/home/carousel/index.tsx
+++ client/src/pages/home/carousel/index.tsx
@@ -1,60 +1,75 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useContext } from "react";
 import Block from "styledComps/block";
 import Header from "./header";
 import Content from "./content";
 import Slick from "./slick";
 import { useQuery } from "@apollo/client";
 import { getCarousel } from "graphqlResolvers";
-import { ICarouselQuery } from "interfaces";
+import { ICarouselQuery, ICarouselContext } from "interfaces";
+import { CarouselContext } from "./store";
+import StoreHOC from "./StoreHOC";
 
 import "./index.scss";
 
-export const Index = () => {
+export interface IProps {}
+export const Index: React.FC<IProps> = () => {
   const { data, loading, error } = useQuery<ICarouselQuery>(getCarousel);
-  console.log("Index -> data", data);
-  const [headerList, setHeaderList] = useState<Array<string>>([]);
-  const [activeTab, setActiveTab] = useState(headerList[0]);
+  const {
+    state: { activeItem, activeTab, tabList },
+    dispatch,
+  } = useContext<ICarouselContext>(CarouselContext);
+
+  const counter = useRef(0);
+  useEffect(() => {
+    dispatch.setLoading(loading);
+    if (error) dispatch.setError(error.message);
+    if (!error) dispatch.setError("");
+    if (data?.getCarousel) {
+      const tempArr: Array<string> = [];
+      data.getCarousel.forEach((element) => {
+        const { list, mainTitle } = element;
+        if (list.length > 0) {
+          tempArr.push(mainTitle);
+        }
+      });
+      dispatch.setTabList(tempArr);
+      dispatch.setActiveTab(tempArr[0]);
+      dispatch.setActiveItem(data.getCarousel[0].list[0]);
+      dispatch.setData(data.getCarousel);
+    }
+  }, [data, loading, error]);
 
   useEffect(() => {
-    const tempArr: Array<string> = [];
-    if (data) {
-      const { getCarousel } = data;
-      if (getCarousel) {
-        getCarousel.forEach((element) => {
-          const { list, mainTitle } = element;
-          if (list.length > 0) {
-            tempArr.push(mainTitle);
-            list.forEach((elem) => {});
-          }
-        });
-      }
-      setHeaderList([...tempArr]);
-      setActiveTab(tempArr[0]);
+    counter.current = 0;
+    if (activeTab && data) {
+      const index = tabList.indexOf(activeTab);
+      dispatch.setActiveItem(data.getCarousel[index].list[0]);
+      const interval = setInterval(() => {
+        counter.current = counter.current + 1;
+        if (counter.current === data.getCarousel[index].list.length) {
+          counter.current = 0;
+        }
+        dispatch.setActiveItem(data.getCarousel[index].list[counter.current]);
+      }, 5000);
+      return () => clearInterval(interval);
     }
-  }, [data]);
+  }, [activeTab]);
+
   return (
-    <Block col className="carouselContainer">
-      <img
-        className="backdropImg"
-        src="https://images.hepsiburada.net/banners/s/0/1920-540/backgroundImage2118_20200825012817.png"
-        alt="a"
-      />
-      <Header
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-        headerList={headerList}
-      />
-      {data?.getCarousel && <Content data={data.getCarousel[0].list[0]} />}
-      {data?.getCarousel && (
-        <Slick
-          data={data.getCarousel}
-          activeTab={activeTab}
-          setActiveTab={setActiveTab}
-          headerList={headerList}
-        />
-      )}
+    <Block
+      col
+      className={
+        activeItem?.isDark
+          ? "carouselContainer isDarkCarousel"
+          : "carouselContainer"
+      }
+    >
+      <img className="backdropImg" src={activeItem?.backImg} alt="backdrop" />
+      <Header />
+      {activeItem && <Content data={activeItem} />}
+      <Slick counter={counter} />
     </Block>
   );
 };
 
-export default Index;
+export default StoreHOC(Index);
