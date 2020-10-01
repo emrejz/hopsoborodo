6969c4b889dd0a468a1eafbe553b6aa3c2628a6e
emre kayacan <emre.kayacan@msn.com>
2020-10-01 13:02:14 +0300

    component status comp created

---
+++ client/src/components/compStatus/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Block from "styledComps/block";
+import Text from "styledComps/text";
+import SpinnerBall from "./spinnerBall";
+
+import "./index.scss";
+interface IProps {
+  loading?: boolean;
+  error?: string;
+}
+const CompStatus: React.FC<IProps> = ({ error, loading }) => {
+  return (
+    <Block
+      as="nav"
+      pad="24px"
+      jc="center"
+      ai="center"
+      bc={error ? "--compStatusErrorBC" : "--primary"}
+      className="compStatusContainer"
+    >
+      {loading && <SpinnerBall animationDelay="0s" />}
+      {loading && <SpinnerBall animationDelay=".2s" />}
+      {loading && <SpinnerBall animationDelay=".4s" />}
+      {error && (
+        <Text size="3rem">
+          {"Something went wrong. Please refresh the page and try again. (" +
+            error +
+            ")"}
+        </Text>
+      )}
+    </Block>
+  );
+};
+
+export default CompStatus;
---
+++ client/src/components/compStatus/spinnerBall/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Block from "styledComps/block";
+
+import "./index.scss";
+interface IProps {
+  animationDelay: string;
+}
+const SpinnerBall: React.FC<IProps> = ({ animationDelay }) => {
+  return (
+    <Block wid="18px" hei="18px" ai="center" jc="center">
+      <Block
+        bc="--orange"
+        className="ballContainer"
+        style={{ animationDelay }}
+      ></Block>
+    </Block>
+  );
+};
+
+export default SpinnerBall;
