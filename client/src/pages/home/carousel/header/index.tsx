01af216cc0c6bd09c1a8911bb22db56e382b012f
emre kayacan <emre.kayacan@msn.com>
2020-10-03 02:45:58 +0300

    edited carousel header css

--- client/src/pages/home/carousel/header/index.tsx
+++ client/src/pages/home/carousel/header/index.tsx
@@ -3,13 +3,15 @@ import Block from "styledComps/block";
 import { CarouselContext } from "../store";
 import HeaderItem from "./headerItem";
 
+import "./index.scss";
+
 interface IProps {}
 const Index: React.FC<IProps> = () => {
   const {
     state: { tabList },
   } = useContext(CarouselContext);
   return (
-    <Block flex="1" jc="center" pt="18px" pb="13px">
+    <Block className="carouselHeaderContainer">
       {tabList.map((item, index) => (
         <HeaderItem key={index} mainTitle={item} />
       ))}
