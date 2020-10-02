c3124c4015d7f0e9fbc8a75fadecdc698767a51f
emre kayacan <emre.kayacan@msn.com>
2020-10-03 02:56:18 +0300

    edited carousel for mobile and no need counter

--- client/src/pages/home/carousel/index.tsx
+++ client/src/pages/home/carousel/index.tsx
@@ -1,7 +1,8 @@
-import React, { useRef, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import Block from "styledComps/block";
 import Header from "./header";
-import Content from "./content";
+import ContentDesktop from "./contentDesktop";
+import ContentMobile from "./contentMobile";
 import Slick from "./slick";
 import CompStatus from "components/compStatus";
 import { useQuery } from "@apollo/client";
@@ -9,6 +10,7 @@ import { getCarousel } from "graphqlResolvers";
 import { ICarouselQuery, ICarouselContext } from "interfaces";
 import { CarouselContext } from "./store";
 import StoreHOC from "./StoreHOC";
+import { useWindowSize } from "helpers/hooks/useWindowSize";
 
 import "./index.scss";
 
@@ -16,16 +18,16 @@ export interface IProps {}
 export const Index: React.FC<IProps> = () => {
   const { data, loading, error } = useQuery<ICarouselQuery>(getCarousel);
   const {
-    state: { activeItem, activeTab, tabList },
+    state: { activeItem, selectedList },
     dispatch,
   } = useContext<ICarouselContext>(CarouselContext);
-
-  const counter = useRef(0);
+  const { width } = useWindowSize();
   useEffect(() => {
     dispatch.setLoading(loading);
     if (error) dispatch.setError(error.message);
     if (!error) dispatch.setError("");
     if (data?.getCarousel) {
+      dispatch.setData(data.getCarousel);
       const tempArr: Array<string> = [];
       data.getCarousel.forEach((element) => {
         const { list, mainTitle } = element;
@@ -36,25 +38,8 @@ export const Index: React.FC<IProps> = () => {
       dispatch.setTabList(tempArr);
       dispatch.setActiveTab(tempArr[0]);
       dispatch.setActiveItem(data.getCarousel[0].list[0]);
-      dispatch.setData(data.getCarousel);
     }
   }, [data, loading, error]);
-
-  useEffect(() => {
-    counter.current = 0;
-    if (activeTab && data) {
-      const index = tabList.indexOf(activeTab);
-      dispatch.setActiveItem(data.getCarousel[index].list[0]);
-      const interval = setInterval(() => {
-        counter.current = counter.current + 1;
-        if (counter.current === data.getCarousel[index].list.length) {
-          counter.current = 0;
-        }
-        dispatch.setActiveItem(data.getCarousel[index].list[counter.current]);
-      }, 5000);
-      return () => clearInterval(interval);
-    }
-  }, [activeTab]);
 
   return (
     <>
@@ -64,8 +49,8 @@ export const Index: React.FC<IProps> = () => {
         <Block
           col
           className={
-            activeItem?.isDark
-              ? "carouselContainer isDarkCarousel"
+            activeItem?.isDark && width && width > 800
+              ? "carouselContainer carouselDarkBackdrop"
               : "carouselContainer"
           }
         >
@@ -75,8 +60,14 @@ export const Index: React.FC<IProps> = () => {
             alt="backdrop"
           />
           <Header />
-          {activeItem && <Content data={activeItem} />}
-          <Slick counter={counter} />
+          {width && width > 800 ? (
+            <Block col>
+              <ContentDesktop />
+              <Slick />
+            </Block>
+          ) : (
+            <ContentMobile selectedList={selectedList} />
+          )}
         </Block>
       )}
     </>
