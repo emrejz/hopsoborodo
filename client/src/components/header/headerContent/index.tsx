83fb1b2b63f94259a02f97a22eb7ece6d424e055
emre kayacan <emre.kayacan@msn.com>
2020-10-03 03:09:21 +0300

    headerContent comp height changed

--- client/src/components/header/headerContent/index.tsx
+++ client/src/components/header/headerContent/index.tsx
@@ -6,7 +6,7 @@ import MasterBar from "./masterBar";
 interface IProps {}
 const HeaderContent: React.FC<IProps> = () => {
   return (
-    <Block as="header" col hei="110px" bc="--primary">
+    <Block as="header" col bc="--primary" pt="14px" pb="16px">
       <TopLinks />
       <MasterBar />
     </Block>
