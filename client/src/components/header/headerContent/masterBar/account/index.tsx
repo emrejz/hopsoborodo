d1574ef81fc34c65f557a2dbbfc5788940767c63
emre kayacan <emre.kayacan@msn.com>
2020-10-03 03:02:48 +0300

    header acc buttons edited for mobile

--- client/src/components/header/headerContent/masterBar/account/index.tsx
+++ client/src/components/header/headerContent/masterBar/account/index.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useRef } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import Block from "styledComps/block";
 import ShopCart from "./shopCart";
 import Sign from "./sign";
@@ -9,7 +8,7 @@ import "./index.scss";
 interface IProps {}
 const Account: React.FC<IProps> = () => {
   return (
-    <Block wid="309px" hei="48px" jc="space-between">
+    <Block className="headerAccContainer">
       <Sign />
       <ShopCart />
     </Block>
