bb207bea9e3fa30f9db9edd19d622e332a8220bb
emre kayacan <emre.kayacan@msn.com>
2020-10-21 22:17:21 +0300

    imported darkMode comp on app

--- client/src/components/app/index.tsx
+++ client/src/components/app/index.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 import Root from "router";
+import DarkMode from "../darkMode";
 import { SessionProvider } from "stores/session";
 import { CarouselProvider } from "stores/carousel";
 
 import "./index.scss";
 
 function App() {
+  const [isDark, setIsDark] = useState(false)
   return (
-    <div className="App">
+    <div className={isDark ? "appContainer darkMode":"appContainer"}>
       <SessionProvider>
         <CarouselProvider>
+        <DarkMode isDark={isDark} setIsDark={setIsDark}/>
           <Root />
         </CarouselProvider>
       </SessionProvider>
